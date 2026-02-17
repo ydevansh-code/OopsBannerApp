cb5044469677eb5b23e9c1ce1b7962846e70540f
ydevansh-code <dy6.idk@gmail.com>
2026-02-17 15:27:39 +0530

    Fix: Revert Stage 1 to basic print statement
    Corrected a mistake where Stage 4 code was accidentally uploaded into the Stage 1 folder. Reverted the file to the original simple 'OOPS' console output as per the assignment requirements for the first use case.

--- OOPS-Banner-Project/Stage1/BannerApp.java
+++ OOPS-Banner-Project/Stage1/BannerApp.java
@@ -1,43 +1,5 @@
 public class BannerApp {
-
-    public static String[] getO() {
-        return new String[] {
-            " *** ",
-            "*   *",
-            "*   *",
-            "*   *",
-            " *** "
-        };
-    }
-
-    public static String[] getP() {
-        return new String[] {
-            "**** ",
-            "*  *",
-            "**** ",
-            "* ",
-            "* "
-        };
-    }
-
-    public static String[] getS() {
-        return new String[] {
-            " ****",
-            " * ",
-            " ****",
-            "    *",
-            " **** "
-        };
-    }
-
     public static void main(String[] args) {
-        String[][] word = { getO(), getO(), getP(), getS() };
-
-        for (int i = 0; i < 5; i++) {
-            for (String[] letter : word) {
-                System.out.print(letter[i] + "  ");
-            }
-            System.out.println();
-        }
+        System.out.println("OOPS");
     }
 }
